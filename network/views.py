3acfbf3f07328d0235981e40c5a633a5ddee028d
Christine Hannivan <christinehannivan@gmail.com>
2023-05-17 12:26:18 -0400

    follow unfollow functionality with error

--- network/views.py
+++ network/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from .models import Post, Follower, Like, User
@@ -85,12 +85,56 @@ def register(request):
 def display_profile(request, id):
     user = User.objects.get(pk=id)
     posts = Post.objects.filter(user=user).order_by('-date')
+    # implement ability to check if user is following profile user and set variable to true/false for button display
     followers = Follower.objects.filter(user_followed=user)
     following = Follower.objects.filter(user_following=user)
+    try:
+        is_follower = followers.filter(user=User.objects.get(pk=request.user.id))
+        if len(is_follower) != 0: 
+            user_is_following = True
+        else: 
+            user_is_following = False
+    except: 
+        user_is_following = False
     return render(request, "network/profile.html", {
         "user": user,
         "posts": posts,
         "following": following,
-        "followers": followers
+        "followers": followers,
+        "is_following": user_is_following,
+        "current_user": request.user.id
     })
+
+# class Follower(models.Model):
+#     user_following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_following")
+#     user_followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_followed")
+
+#     def __str__(self):
+#         return f"{self.user_following} is following {self.user_followed}"
+    
+# class Follow(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_who_is_following")
+#     user_follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_who_is_being_followed")
+
+#     def __str__(self):
+#         return f"{self.user_following} is following {self.user_followed}"
+
+def follow(request):
+    user_follow = request.POST['userfollow']
+    current_user = User.objects.get(pk=request.user.id)
+    user_follow_data = User.objects.get(username=user_follow)
+    follow = Follower(user_following=current_user, user_followed=user_follow_data)
+    follow.save()
+    user_id = user_follow_data.id
+    return HttpResponseRedirect(reverse(display_profile), kwargs={'user_id': user_id})
+
+
+def unfollow(request):
+    user_follow = request.POST['userfollow']
+    current_user = User.objects.get(pk=request.user.id)
+    user_follow_data = User.objects.get(username=user_follow)
+    follow = Follower.objects.get(user_following=current_user, user_followed=user_follow_data)
+    follow.delete()
+    user_id = user_follow_data.id
+    return HttpResponseRedirect(reverse(display_profile), kwargs={'user_id': user_id})
 
